d19adae072ef9636e62cc2718507e8d2d1bed087
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-11-17 16:50:56 +0100

    Fix translatable extension with fallback

--- src/Pckg/Database/Entity/Extension/Translatable.php
+++ src/Pckg/Database/Entity/Extension/Translatable.php
@@ -32,8 +32,6 @@ trait Translatable
      */
     protected $translatableLang;
 
-    protected $translatableFallbackLang;
-
     /**
      * @param Lang $lang
      */
@@ -115,6 +113,61 @@ trait Translatable
         return $relation;
     }
 
+    /**
+     * @return mixed
+     */
+    public function translationsFallback(callable $callable = null)
+    {
+        $translaTable = $this->getTable() . $this->getTranslatableTableSuffix;
+        $repository = $this->getRepository();
+
+        $relation = $this->hasMany(
+            (new Entity($repository))->setTable($translaTable)->setAlias($translaTable . '_f')
+        )
+                         ->foreignKey('id')
+                         ->fill('_translations')
+                         ->addSelect(['`' . $translaTable . '`.*'])
+                         ->leftJoin();
+
+        if ($callable) {
+            $query = $relation->getRightEntity()->getQuery();
+
+            Reflect::call(
+                $callable,
+                [
+                    $query,
+                    $relation,
+                    $this,
+                ]
+            );
+
+            $this->addTranslatableFallbackConditionIfNot($relation);
+
+        } else {
+            $this->addTranslatableFallbackCondition($relation);
+
+        }
+
+        return $relation;
+    }
+
+    private function addTranslatableFallbackConditionIfNot(HasMany $relation)
+    {
+        $foundLanguageCondition = false;
+        $query = $relation->getQuery();
+        foreach ($query->getWhere() as $where) {
+            foreach ($where->getChildren() as $key => $child) {
+                if (strpos($key, $this->translatableLanguageField)) {
+                    $foundLanguageCondition = true;
+                }
+            }
+        }
+
+        if (!$foundLanguageCondition) {
+            $this->addTranslatableFallbackCondition($relation);
+        }
+    }
+
     private function addTranslatableConditionIfNot(HasMany $relation)
     {
         $foundLanguageCondition = false;
@@ -139,6 +192,16 @@ trait Translatable
         $relation->where(
             '`' . $translaTable . '`.`' . $this->translatableLanguageField . '`',
             $this->translatableLang->langId()
+        );
+    }
+
+    private function addTranslatableFallbackCondition(HasMany $relation)
+    {
+        $translaTable = $this->getTable() . $this->getTranslatableTableSuffix();
+
+        $relation->where(
+            '`' . $translaTable . '_f`.`' . $this->translatableLanguageField . '`',
+            'en'
         );
     }
 
@@ -167,16 +230,35 @@ trait Translatable
                     ->prependSelect([$this->getTable() . $this->translatableTableSuffix . '.*']);
     }
 
+    public function joinFallbackTranslation(callable $callable = null)
+    {
+        if ($this->translatableLang->langId() == 'en') {
+            return $this;
+        }
+
+        $selects = [];
+        $translaTable = $this->getTable() . $this->translatableTableSuffix;
+        $fields = $this->getRepository()->getCache()->getTableFields($translaTable);
+        foreach ($fields as $field) {
+            if (in_array($field, ['id', $this->translatableLanguageField])) {
+                continue;
+            }
+
+            $translatableField = '`' . $translaTable . '`.`' . $field . '`';;
+            $fallbackField = '`' . $translaTable . '_f`.`' . $field . '`';;
+            $selects[] = 'IF(' . $translatableField . ', ' . $translatableField . ', ' . $fallbackField . ') AS `' . $field . '`';
+        }
+
+        $relation = $this->join($this->translationsFallback($callable));
+
+        $relation->getQuery()->addSelect($selects);
+
+        return $relation;
+    }
+
     public function setTranslatableLang(Lang $lang)
     {
         $this->translatableLang = $lang;
-
-        return $this;
-    }
-
-    public function setFallbackLang(Lang $lang)
-    {
-        $this->translatableFallbackLang = $lang;
 
         return $this;
     }

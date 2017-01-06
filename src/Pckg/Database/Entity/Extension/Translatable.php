22c890470aca05b03fd06db6d4fa3d745ee29030
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-01-06 01:35:42 +0100

    Translatable fixes

--- src/Pckg/Database/Entity/Extension/Translatable.php
+++ src/Pckg/Database/Entity/Extension/Translatable.php
@@ -45,7 +45,7 @@ trait Translatable
      */
     public function initTranslatableExtension()
     {
-        
+
     }
 
     /**
@@ -72,7 +72,7 @@ trait Translatable
     public function getTranslatableForeignKeys(Record $record)
     {
         return [
-            $this->primaryKey                => $record->{$this->primaryKey},
+            $this->primaryKey => $record->{$this->primaryKey},
             $this->translatableLanguageField => $this->translatableLang->langId(),
         ];
     }
@@ -83,12 +83,17 @@ trait Translatable
     public function translations(callable $callable = null)
     {
         $translaTable = $this->getTable() . $this->getTranslatableTableSuffix;
+        $translaTableAlias = $this->getAlias()
+            ? $this->getAlias() . $this->getTranslatableTableSuffix
+            : $translaTable;
         $repository = $this->getRepository();
 
-        $relation = $this->hasMany((new Entity($repository))->setTable($translaTable))
+        $relation = $this->hasMany(
+            (new Entity($repository, $translaTableAlias))->setTable($translaTable)->setAlias($translaTableAlias)
+        )
                          ->foreignKey('id')
                          ->fill('_translations')
-                         ->addSelect(['`' . $translaTable . '`.*'])
+                         ->addSelect(['`' . $translaTableAlias . '`.*'])
                          ->leftJoin();
 
         if ($callable) {
@@ -188,9 +193,12 @@ trait Translatable
     private function addTranslatableCondition(HasMany $relation)
     {
         $translaTable = $this->getTable() . $this->getTranslatableTableSuffix();
+        $translaTableAlias = $this->getAlias()
+            ? $this->getAlias() . $this->getTranslatableTableSuffix()
+            : $translaTable;
 
         $relation->where(
-            '`' . $translaTable . '`.`' . $this->translatableLanguageField . '`',
+            '`' . $translaTableAlias . '`.`' . $this->translatableLanguageField . '`',
             $this->translatableLang->langId()
         );
     }
@@ -198,9 +206,12 @@ trait Translatable
     private function addTranslatableFallbackCondition(HasMany $relation)
     {
         $translaTable = $this->getTable() . $this->getTranslatableTableSuffix();
+        $translaTableAlias = $this->getAlias()
+            ? $this->getAlias() . $this->getTranslatableTableSuffix()
+            : $translaTable;
 
         $relation->where(
-            '`' . $translaTable . '_f`.`' . $this->translatableLanguageField . '`',
+            '`' . $translaTableAlias . '_f`.`' . $this->translatableLanguageField . '`',
             'en'
         );
     }

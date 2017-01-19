63cc4693e5c6541326fdd4bb0d9eb2bfa9805b5b
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-01-19 13:17:08 +0100

    Fix events

--- src/Pckg/Database/Record/Events.php
+++ src/Pckg/Database/Record/Events.php
@@ -2,16 +2,21 @@
 
 trait Events
 {
-    
+
     public function events()
     {
         return [];
     }
 
-    public function trigger($event, $args = [])
+    public function trigger($events, $args = [])
     {
         $args[] = $this;
-        $this->trigger(static::class . '.' . $event, $args);
+        if (!is_array($events)) {
+            $events = [$events];
+        }
+        foreach ($events as $event) {
+            dispatcher()->trigger(static::class . '.' . $event, $args);
+        }
 
         return $this;
     }

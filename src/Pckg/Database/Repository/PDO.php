49363d063ce301f6fee0b8bb97ec68d60d9d795b
Bojan Rajh <schtr4jh@schtr4jh.net>
2020-10-22 12:20:52 +0200

    Create connection by config

--- src/Pckg/Database/Repository/PDO.php
+++ src/Pckg/Database/Repository/PDO.php
@@ -68,7 +68,7 @@ class PDO extends AbstractRepository implements Repository
          * @T00D00 - check for other means of serialization the callback.
          */
         $this->connection = null;
-        $this->connection = RepositoryFactory::initPdoDatabase(config('database.' . $this->name, []), $this->name);;
+        $this->connection = RepositoryFactory::createPdoConnectionByConfig(config('database.' . $this->name, []));
     }
 
     /**

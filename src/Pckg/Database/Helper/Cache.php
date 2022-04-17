a9c78a4093c4e525265536ee6e4d72ca3c14dd79
Bojan Rajh <schtr4jh@schtr4jh.net>
2022-04-17 17:16:18 +0200

    Update

--- src/Pckg/Database/Helper/Cache.php
+++ src/Pckg/Database/Helper/Cache.php
@@ -283,7 +283,7 @@ class Cache extends PckgCache
             ['\\', '/'],
             '_',
             (get_class(app()) . '_' . get_class(env()))
-        ) . '_' . $repository->getName() . '_' . ($repository->getConnection()->uniqueName ?? '') . '.cache';
+        ) . '_' . $repository->getName() . '_' . ($repository?->getConnection()?->uniqueName ?? '') . '.cache';
 
         return $path;
     }
